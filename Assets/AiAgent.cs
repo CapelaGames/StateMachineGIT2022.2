68aa9d112a7194ef2c8695fa0c57eabeff459373
CapelaGames <andrew@Capelagames.com.au>
2022-08-04 18:01:18 +1000

    Starting statemachine

--- Assets/AiAgent.cs
+++ Assets/AiAgent.cs
@@ -8,20 +8,56 @@ using UnityEngine;
 //[SerializeField] private      - Unity can, other classes cannot
 public class AiAgent : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private float speed;
-    void Update()
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _speed;
+
+    [SerializeField] private Transform[] _waypoints; //delare an array
+    [SerializeField] private int _waypointIndex = 0;
+
+    //Array
+    //Pros: They are fast, and simple
+    //Cons: They cannot be resized
+
+    public bool IsPlayerInRange()
     {
-        //
-        //                                                this.transform.position;
-        Vector2 directionToPlayer = player.transform.position - transform.position;
-        //If you want to know the direction from A to B
-        // A ----> B
-        // DirAToB = B - A;
+        if(Vector2.Distance(transform.position, _player.transform.position) < 5f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void ChasePlayer()
+    {
+        MoveToPoint(_player.transform.position);
+    }
+
+
+    public void Patrol()
+    {
+        Vector2 waypointPosition = _waypoints[_waypointIndex].position;
+        MoveToPoint(waypointPosition);
+        if(Vector2.Distance(transform.position, waypointPosition) < 0.1f )
+        {
+            //_waypointIndex = (_waypointIndex+1) % _waypoints.Length;
+            _waypointIndex++;
+        }
+        if(_waypointIndex >= _waypoints.Length)
+        {
+            _waypointIndex = 0;
+        }
+    }
+
+    public void MoveToPoint(Vector2 point)
+    {
+        Vector2 directionToPlayer = point - (Vector2) transform.position;
         if (directionToPlayer.magnitude > 0.1f)
         {
             directionToPlayer.Normalize();
-            directionToPlayer *= speed * Time.deltaTime;
+            directionToPlayer *= _speed * Time.deltaTime;
             transform.position += (Vector3)directionToPlayer;
         }
     }
---
+++ Assets/StateMachine.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AiAgent))]
+public class StateMachine : MonoBehaviour
+{
+    public enum State
+    {
+        Patrol,
+        Aware,
+        Combat,
+        Flee,
+        Sleep,
+        BerryPicking,
+    }
+
+    //The Ai's current state
+    [SerializeField] private State _state;
+
+    private AiAgent _aiAgent;//delare
+
+    private void Start()
+    {
+        //Grabs the first AiAgent (or whatever is in the <>)
+        // and puts it in the variable
+        _aiAgent = GetComponent<AiAgent>();
+
+        NextState();
+    }
+
+    private void NextState()
+    {
+        switch(_state)
+        {
+            case State.Patrol:
+                StartCoroutine(PatrolState());
+                break;
+            case State.Combat:
+                StartCoroutine(CombatState());
+                break;
+            default:
+                Debug.LogWarning("_state doesnt exist withing NextState function, stopping statemachine");
+                break;
+        }
+    }
+
+    private IEnumerator PatrolState()
+    {
+        Debug.Log("Patrol: Enter");
+        while(_state == State.Patrol)
+        {
+            _aiAgent.Patrol();
+            if(_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Combat;
+            }
+
+            yield return null; //wait a single frame
+        }
+        Debug.Log("Patrol: Exit");
+        NextState();
+    }
+
+    private IEnumerator CombatState()
+    {
+        Debug.Log("Combat: Enter");
+        while (_state == State.Combat)
+        {
+            _aiAgent.ChasePlayer();
+
+            if (!_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Patrol;
+            }
+            yield return null; //wait a single frame
+        }
+        Debug.Log("Combat: Exit");
+        NextState();
+    }
+}
