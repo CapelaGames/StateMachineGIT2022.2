82c19405c1b21241a66fc24d1eed3ac77531af27
CapelaGames <andrew@Capelagames.com.au>
2022-08-11 13:22:31 +1000

    statemachine update

--- Assets/AiAgent.cs
+++ Assets/AiAgent.cs
@@ -15,15 +15,66 @@ public class AiAgent : MonoBehaviour
     //Array
     //Store many values as a single variable
     //Pros: fast
-    //Cons: set size
+    //Cons: cannot be resized
 
-    void Update()
+    public bool IsPlayerInRange()
+    {
+       if(Vector2.Distance(transform.position, _player.transform.position) < 5f)
+        {
+            return true;
+        }
+       else
+        {
+            return false;
+        }
+    }
+
+    //while loop,        keeps looping until the statement is false
+    //do while <-- later
+    //for                loops for when you have a set ammount of times you want to loop
+    //foreach <-- later
+
+
+    //if the player is not in range -> then search
+    public void Search()
+    {
+        int closestIndex = -1;
+        float closestDistance = float.MaxValue;
+
+        //   initializer        condition             iterator 
+        for (int index = 0; index < _waypoints.Length; index++)
+        {
+            float currentDistance = Vector2.Distance(_waypoints[index].position, transform.position);
+            if (currentDistance < closestDistance)
+            {
+                closestDistance = currentDistance;
+                closestIndex = index;
+            }
+        }
+
+        _waypointIndex = closestIndex;
+    }
+
+    public void ChasePlayer()
+    {
+
+        MoveToPoint(_player.transform.position);
+    }
+
+    public void Patrol()
     {
         Vector2 waypointPosition = _waypoints[_waypointIndex].position;
         MoveToPoint(waypointPosition);
         if(Vector2.Distance(transform.position, waypointPosition) < 0.1f)
         {
-            _waypointIndex++;
+            _waypointIndex++; // increase waypointIndex by 1
+
+            //_waypointIndex = _waypointIndex % _waypoints.Length;
+        }
+
+        if(_waypointIndex >= _waypoints.Length)
+        {
+            _waypointIndex = 0;
         }
 
         //MoveToPoint(_player.transform.position);
@@ -37,5 +88,21 @@ public class AiAgent : MonoBehaviour
             directionToPlayer *= _speed * Time.deltaTime;
             transform.position += (Vector3)directionToPlayer;
         }
+    }
+
+    public void OrbitPlayer()
+    {
+        Orbit(_player.transform.position);
+    }
+
+    [SerializeField] float orbitDistance = 2f;
+    void Orbit(Vector2 point)
+    {
+       // Vector2 pointDirection =  point - (Vector2)transform.position;
+        //Vector2 closerPoint = pointDirection.normalized * orbitDistance;
+
+        transform.RotateAround(point, Vector3.forward, _speed * 10 * Time.deltaTime);
+
+        Vector2.
     }
 }
--- Assets/StateMachine.cs
+++ Assets/StateMachine.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AiAgent))]
 public class StateMachine : MonoBehaviour
 {
     //comma separated list of identifier
@@ -10,14 +11,18 @@ public class StateMachine : MonoBehaviour
         Patrol, 
         Chase,
         Search,
+        Orbit,
         BerryPicking
    }
-
     //The AI's current state
     [SerializeField] private State _state;
-
+    private AiAgent _aiAgent;
     private void Start()
     {
+        //Grabs the first AiAgent it finds (or whatever is in the <>)
+        //and stores it in the variable
+        _aiAgent = GetComponent<AiAgent>();
+
         NextState();
     }
     private void NextState()
@@ -27,34 +32,63 @@ public class StateMachine : MonoBehaviour
             case State.Patrol:
                 StartCoroutine(PatrolState());
                 break;
+            case State.Chase:
+                StartCoroutine(ChaseState());
+                break;
+            case State.Orbit:
+                StartCoroutine(OrbitState());
+                break;
             case State.BerryPicking:
                 StartCoroutine(BerryPickingState());
                 break;
             default:
-                Debug.LogWarning("State does not exsit in NextState function, stopping statemachine");
+                Debug.LogWarning("State does not exist in NextState function, stopping statemachine");
                 break;
         }
-    
- /*       if(_state == State.Patrol)
-        {
-            StartCoroutine(PatrolState());
-        }
-        else if(_state == State.BerryPicking)
-        {
-            StartCoroutine(BerryPickingState());
-        }*/
-        
     }
 
     private IEnumerator PatrolState()
     {
         Debug.Log("Patrol: Enter");
+        _aiAgent.Search();
         while (_state == State.Patrol)
         {
-            //Our code runs here
+            _aiAgent.Patrol();
+            if(_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Chase;
+            }
             yield return null; //wait a single frame
         }
         Debug.Log("Patrol: Exit");
+        NextState();
+    }
+
+    private IEnumerator OrbitState()
+    {
+        Debug.Log("Orbit: Enter");
+        while (_state == State.Orbit)
+        {
+            _aiAgent.OrbitPlayer();
+            yield return null; //wait a single frame
+        }
+        Debug.Log("Orbit: Exit");
+        NextState();
+    }
+    private IEnumerator ChaseState()
+    {
+        Debug.Log("Chase: Enter");
+        while (_state == State.Chase)
+        {
+
+            _aiAgent.ChasePlayer();
+            if (!_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Patrol;
+            }
+            yield return null; //wait a single frame
+        }
+        Debug.Log("Chase: Exit");
         NextState();
     }
 
